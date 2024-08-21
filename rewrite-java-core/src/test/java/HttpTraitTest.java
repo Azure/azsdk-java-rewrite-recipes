08b9e7ca53c5fec638bd18f4297eaa0dd36ef19c
Annabelle Mittendorf Smith <annabellemittendorf@gmail.com>
2024-08-21 07:37:21 +0200

    HttpTraitTest tidied-up

--- rewrite-java-core/src/test/java/HttpTraitTest.java
+++ rewrite-java-core/src/test/java/HttpTraitTest.java
@@ -9,8 +9,7 @@ import static org.openrewrite.java.Assertions.java;
 /**
  * HttpTraitTest tests interface migration from com.azure.core.client.traits.HttpTrait
  * to io.clientcore.core.models.traits.HttpTrait.
- * Tests:
- * Simple method renaming with declarative recipe.
+ * Tests simple method renaming with declarative recipe.
  * CURRENTLY UNFINISHED. All tests for HttpTrait migration will be added here.
  * @author Annabelle Mittendorf Smith
  */
@@ -22,7 +21,6 @@ public class HttpTraitTest implements RewriteTest {
      */
     @Override
     public void defaults(RecipeSpec spec) {
-        // Declarative recipes defined in rewrite.yml
         spec.recipes(new ChangeMethodName("com.azure.core.client.traits.HttpTrait retryOptions(..)",
                 "httpRetryOptions",
                 true, false),
@@ -36,95 +34,10 @@ public class HttpTraitTest implements RewriteTest {
     }
 
     /**
-     * Basic implementation of starting interface com.azure.core.client.traits.HttpTrait
-     */
-    @Language("java") String blankBefore = "import com.azure.core.client.traits.HttpTrait;\n" +
-            "import com.azure.core.http.HttpClient;\n" +
-            "import com.azure.core.http.HttpPipeline;\n" +
-            "import com.azure.core.http.policy.HttpLogOptions;\n" +
-            "import com.azure.core.http.policy.HttpPipelinePolicy;\n" +
-            "import com.azure.core.http.policy.RetryOptions;\n" +
-            "import com.azure.core.util.ClientOptions;\n" +
-            "\n" +
-            "public class BlankHttpTrait implements HttpTrait<BlankHttpTrait> {\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait httpClient(HttpClient httpClient) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait pipeline(HttpPipeline pipeline) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait addPolicy(HttpPipelinePolicy pipelinePolicy) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait retryOptions(RetryOptions retryOptions) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait httpLogOptions(HttpLogOptions logOptions) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait clientOptions(ClientOptions clientOptions) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "}";
-
-    /**
-     * Basic implementation of target interface io.clientcore.core.models.traits.HttpTrait
-     */
-    @Language("java") String blankAfter = "import io.clientcore.core.http.client.HttpClient;\n" +
-            "import io.clientcore.core.http.models.HttpLogOptions;\n" +
-            "import io.clientcore.core.http.models.HttpRedirectOptions;\n" +
-            "import io.clientcore.core.http.models.HttpRetryOptions;\n" +
-            "import io.clientcore.core.http.pipeline.HttpPipeline;\n" +
-            "import io.clientcore.core.http.pipeline.HttpPipelinePolicy;\n" +
-            "import io.clientcore.core.models.traits.HttpTrait;\n" +
-            "\n" +
-            "public class BlankHttpTrait implements HttpTrait<BlankHttpTrait> {\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait httpClient(HttpClient client) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait httpPipeline(HttpPipeline pipeline) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait addHttpPipelinePolicy(HttpPipelinePolicy pipelinePolicy) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait httpRetryOptions(HttpRetryOptions retryOptions) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait httpLogOptions(HttpLogOptions logOptions) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "\n" +
-            "    @Override\n" +
-            "    public BlankHttpTrait httpRedirectOptions(HttpRedirectOptions redirectOptions) {\n" +
-            "        return null;\n" +
-            "    }\n" +
-            "}";
-    /**
      * Test that non-interface methods with the same name are not altered.
      */
     @Test
-    void doesNotChangeNonInheritedMethods() {
+    void testDoesNotChangeNonInheritedMethods() {
         @Language("java")String noChange = "public class TestClass {\n" +
                 "    public void retryOptions() {\n" +
                 "    }\n" +
@@ -144,6 +57,46 @@ public class HttpTraitTest implements RewriteTest {
      */
     @Test
     void declarativeRenameMethodsSuccessful() {
+        @Language("java") String before = "import com.azure.core.client.traits.HttpTrait;\n" +
+                "import com.azure.core.http.HttpClient;\n" +
+                "import com.azure.core.http.HttpPipeline;\n" +
+                "import com.azure.core.http.policy.HttpLogOptions;\n" +
+                "import com.azure.core.http.policy.HttpPipelinePolicy;\n" +
+                "import com.azure.core.http.policy.RetryOptions;\n" +
+                "import com.azure.core.util.ClientOptions;\n" +
+                "\n" +
+                "public class BlankHttpTrait implements HttpTrait<BlankHttpTrait> {\n" +
+                "    @Override\n" +
+                "    public BlankHttpTrait httpClient(HttpClient httpClient) {\n" +
+                "        return null;\n" +
+                "    }\n" +
+                "\n" +
+                "    @Override\n" +
+                "    public BlankHttpTrait pipeline(HttpPipeline pipeline) {\n" +
+                "        return null;\n" +
+                "    }\n" +
+                "\n" +
+                "    @Override\n" +
+                "    public BlankHttpTrait addPolicy(HttpPipelinePolicy pipelinePolicy) {\n" +
+                "        return null;\n" +
+                "    }\n" +
+                "\n" +
+                "    @Override\n" +
+                "    public BlankHttpTrait retryOptions(RetryOptions retryOptions) {\n" +
+                "        return null;\n" +
+                "    }\n" +
+                "\n" +
+                "    @Override\n" +
+                "    public BlankHttpTrait httpLogOptions(HttpLogOptions logOptions) {\n" +
+                "        return null;\n" +
+                "    }\n" +
+                "\n" +
+                "    @Override\n" +
+                "    public BlankHttpTrait clientOptions(ClientOptions clientOptions) {\n" +
+                "        return null;\n" +
+                "    }\n" +
+                "}";
+
         @Language("java") String after = "import com.azure.core.client.traits.HttpTrait;\n" +
                 "import com.azure.core.http.HttpClient;\n" +
                 "import com.azure.core.http.HttpPipeline;\n" +
@@ -185,7 +138,7 @@ public class HttpTraitTest implements RewriteTest {
                 "}";
 
         rewriteRun(
-                java(blankBefore,after)
+                java(before,after)
         );
     }
 
