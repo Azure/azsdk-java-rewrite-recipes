b5dd8f5e7d2542a09a89c1c004fe87e8fd445dd9
Casperdeww <scottreganclark@gmail.com>
2024-09-09 18:43:18 +1200

    Commit 2

--- rewrite-java-core/src/main/java/com/azure/recipes/v2recipes/HeaderRecipe.java
+++ rewrite-java-core/src/main/java/com/azure/recipes/v2recipes/HeaderRecipe.java
@@ -1,10 +1,13 @@
 package com.azure.recipes.v2recipes;
 
+import org.jetbrains.annotations.NotNull;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.NlsRewrite;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.TypeTree;
 
 public class HeaderRecipe  extends Recipe {
 
@@ -33,6 +36,26 @@ public class HeaderRecipe  extends Recipe {
     }
 
     private static class ChangeHttpHeaderNameVisitor extends JavaIsoVisitor<ExecutionContext> {
+
+        @Override
+        public J.@NotNull Identifier visitIdentifier(J.@NotNull Identifier ident, @NotNull ExecutionContext contxt) {
+            J.Identifier visitedId = super.visitIdentifier(ident, contxt);
+
+            if (visitedId.getType() != null && "com.azure.core.http.HttpHeaderName".equals(visitedId.getType().toString())) {
+                return visitedId.withType(TypeTree.build("io.clientcore.core.http.models.HttpHeaderName").getType());
+            }
+            return visitedId;
+        }
+
+        @Override
+        public J.Import visitImport(J.Import imp, ExecutionContext contxt) {
+            J.Import vistedImp = super.visitImport(imp, contxt);
+
+            if ("com.azure.core.http.HttpHeaderName".equals(vistedImp.getTypeName())) {
+                return vistedImp.withTypeName("io.clientcore.core.http.models.HttpHeaderName");
+            }
+            return vistedImp;
+        }
 
     }
 }
