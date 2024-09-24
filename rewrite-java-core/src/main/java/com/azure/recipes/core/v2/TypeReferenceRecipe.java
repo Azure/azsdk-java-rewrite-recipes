e4fd9e4833a9c8e2404eec9d4f67004c1fb94bd1
Ali <86838752+AliSoltanianFJ@users.noreply.github.com>
2024-09-24 13:05:10 +1200

    Updated TypeReference recipe to use OpenRewrite type info

--- rewrite-java-core/src/main/java/com/azure/recipes/core/v2/TypeReferenceRecipe.java
+++ rewrite-java-core/src/main/java/com/azure/recipes/core/v2/TypeReferenceRecipe.java
@@ -1,13 +1,17 @@
 package com.azure.recipes.core.v2;
 
 import org.jetbrains.annotations.NotNull;
-import org.openrewrite.*;
+import org.openrewrite.Cursor;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
 import org.openrewrite.java.tree.TypeTree;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+
+import java.util.List;
 
 /**
  * Recipe to convert TypeReference to ParameterizedType and remove TypeReference import statements.
@@ -48,28 +52,45 @@ public class TypeReferenceRecipe extends Recipe {
 
             if (!alreadyTransformed && visitedNewClass.toString().contains("TypeReference")) {
                 // Extract type from generic type in TypeReference declaration
-                Pattern pattern = Pattern.compile("<([\\w.]+)<([\\w.]+)(?:,\\s*([\\w.]+))?>>");
-                Matcher matcher = pattern.matcher(visitedNewClass.toString());
                 String genType = null;
                 String type = null;
                 String type2 = null;
-                if(matcher.find()){ // Check if using generic type
-                    genType = matcher.group(1);
-                    type = matcher.group(2);
-                    type2 = matcher.group(3);
-                }
+                List<JavaType> fullType = null;
+                if (visitedNewClass.getClazz() == null){return visitedNewClass;}
 
-                JavaTemplate methodRawTypeTemplate = JavaTemplate.builder("@Override public java.lang.reflect.Type getRawType() { return " + extractTypeArgument(visitedNewClass.toString()) + ".class; }").build();
+                if(visitedNewClass.getClazz().getType() != null){
+                    try {
+                        fullType = ((JavaType.Parameterized) visitedNewClass.getClazz().getType()).getTypeParameters();
+                        if (fullType.get(0) instanceof JavaType.Parameterized) { // Check if using parameterized type
+                            type = ((JavaType.Class)((JavaType.Parameterized) fullType.get(0)).getTypeParameters().get(0)).getClassName();
+                            if (((JavaType.Parameterized) fullType.get(0)).getTypeParameters().size() > 1) {
+                                type2 = ((JavaType.Class)((JavaType.Parameterized) fullType.get(0)).getTypeParameters().get(1)).getClassName();
+                            }
+                            genType = ((JavaType.Parameterized) fullType.get(0)).getClassName();
+                        } else {
+                            genType = ((JavaType.Class)fullType.get(0)).getClassName();
+                        }
+                    } catch (ClassCastException e) { // OpenRewrite has a bug where parameterized new-classes contained in method arguments can't be parsed, so extract type information using String manipulation instead
+                        genType = visitedNewClass.getClazz().toString().split("<")[1];
+                        if (visitedNewClass.getClazz().toString().contains(",")) {
+                            type = visitedNewClass.getClazz().toString().split("<")[2].replace(">", "").split(",")[0];
+                            type2 = visitedNewClass.getClazz().toString().split("<")[2].replace(">", "").split(",")[1];
+                        }else {
+                            type = visitedNewClass.getClazz().toString().split("<")[2].replace(">", "");
+                        }
+                    }
+                }
+                JavaTemplate methodRawTypeTemplate = JavaTemplate.builder("@Override public Type getRawType() { return " + extractTypeArgument(visitedNewClass.toString()) + ".class; }").build();
                 if (genType!=null){
-                    methodRawTypeTemplate = JavaTemplate.builder("@Override public java.lang.reflect.Type getRawType() { return " + genType + ".class; }").build();
+                    methodRawTypeTemplate = JavaTemplate.builder("@Override public Type getRawType() { return " + genType + ".class; }").build();
                 }
-                JavaTemplate methodActualTypeTemplate = JavaTemplate.builder("@Override public java.lang.reflect.Type[] getActualTypeArguments() { return new java.lang.reflect.Type[] {}; }").build();
+                JavaTemplate methodActualTypeTemplate = JavaTemplate.builder("@Override public Type[] getActualTypeArguments() { return new Type[] {}; }").build();
                 if (type != null){
-                    methodActualTypeTemplate = JavaTemplate.builder("@Override public java.lang.reflect.Type[] getActualTypeArguments() { return new java.lang.reflect.Type[] {  " + type + ".class  }; }").build();
+                    methodActualTypeTemplate = JavaTemplate.builder("@Override public Type[] getActualTypeArguments() { return new Type[] {  " + type + ".class  }; }").build();
                 }
-                JavaTemplate methodOwnerTypeTemplate = JavaTemplate.builder("@Override public java.lang.reflect.Type getOwnerType() { return null; }").build();
+                JavaTemplate methodOwnerTypeTemplate = JavaTemplate.builder("@Override public Type getOwnerType() { return null; }").build();
                 if (type2 != null){ // If TypeReference uses Map<T,V> or any other class with the same structure like Foo<T,V>
-                    methodActualTypeTemplate = JavaTemplate.builder("@Override public java.lang.reflect.Type[] getActualTypeArguments() { return new java.lang.reflect.Type[] {  " + type + ".class," + type2 + ".class  }; }").build();
+                    methodActualTypeTemplate = JavaTemplate.builder("@Override public Type[] getActualTypeArguments() { return new Type[] {  " + type + ".class," + type2 + ".class  }; }").build();
                 }
                 // Apply Templates (add methods to body)
 
@@ -120,7 +141,7 @@ public class TypeReferenceRecipe extends Recipe {
             J.VariableDeclarations visitedDeclarations = super.visitVariableDeclarations(multiVariable, executionContext);
             if (visitedDeclarations.toString().contains("TypeReference")
                     && visitedDeclarations.toString().contains("ParameterizedType")) {
-                return visitedDeclarations.withTypeExpression(TypeTree.build(" java.lang.reflect.Type"));
+                return visitedDeclarations.withTypeExpression(TypeTree.build(" Type"));
             }
             return visitedDeclarations;
         }
